79fafcc84664a4d7e4d89fedb7fab55210503c5f
Maria Lemos <oi@marialemos.com>
2023-02-17 13:00:58 -0300

    feat: add contextMenu

---
+++ app/src/components/contextMenu/contextMenu.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { ReactComponent as Ellipses } from "../../assets/icon-vertical-ellipsis.svg";
+
+export const ContextMenu: React.FC<{
+  contextName: "board" | "task";
+  editAction: () => void;
+  deleteAction: () => void;
+}> = ({ contextName, editAction, deleteAction }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <StyledMenuSecondary>
+      <ButtonToggleVisibility
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <Ellipses />
+      </ButtonToggleVisibility>
+      {isOpen && (
+        <Menu>
+          <MenuOption onClick={editAction}>Edit {contextName}</MenuOption>
+          <DeleteOption onClick={deleteAction}>
+            Delete {contextName}
+          </DeleteOption>
+        </Menu>
+      )}
+    </StyledMenuSecondary>
+  );
+};
+
+const StyledMenuSecondary = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: right;
+`;
+const ButtonToggleVisibility = styled.button`
+  background-color: transparent;
+  border: none;
+`;
+const Menu = styled.ul`
+  background-color: ${({ theme }) => theme.default.bg};
+  width: 10rem;
+  position: absolute;
+  list-style: none;
+  right: 0rem;
+  top: 3rem;
+`;
+const MenuOption = styled.li`
+  padding: 1rem;
+  color: ${({ theme }) => theme.mediumGrey};
+`;
+const DeleteOption = styled(MenuOption)`
+  color: ${({ theme }) => theme.red};
+`;
--- app/src/components/header/header.tsx
+++ app/src/components/header/header.tsx
@@ -1,26 +1,51 @@
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { ReactComponent as IconAdd } from "../../assets/icon-add-task-mobile.svg";
 import { ReactComponent as LogoMobile } from "../../assets/logo-mobile.svg";
 import { Button } from "../../commons/button";
 import useIsMobile from "../../helpers/useIsMobile";
+import { ContextMenu } from "../contextMenu/contextMenu";
+import { PageTitle } from "./pageTitle";
+
 export const Header: React.FC = () => {
   const isMobile = useIsMobile();
+  const theme = useTheme() as Theme;
+  const Logo = theme.logo;
+
   return (
     <StyledHeader>
-      {isMobile && <LogoMobile />}
-      <h2>Platform Launch</h2>
-      <Button variant="primary" size="small">
+      {isMobile ? <LogoMobile /> : <Logo />}
+
+      <PageTitle />
+      <StyledButton variant="primary" size="small">
         <IconAdd />
-      </Button>
+      </StyledButton>
+      <ContextMenu
+        editAction={console.log}
+        contextName="board"
+        deleteAction={() => {}}
+      />
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
-  padding: 1rem;
+  padding: 1.33rem;
   background-color: ${({ theme }) => theme.darkGrey};
-  height: 4rem;
-  display: flex;
-  justify-content: space-between;
+
+  display: grid;
+  grid-gap: 1.3rem;
   align-items: center;
+  grid-template-columns: 2rem 1fr 4rem 1rem;
+  @media (min-width: 768px) {
+    grid-template-columns: 11rem 1fr 4rem 1rem;
+  }
+  @media (min-width: 1000px) {
+    padding: 1.5rem;
+  }
+`;
+const StyledButton = styled(Button)`
+  @media (max-width: 767px) {
+    height: 2.6rem;
+    padding: 0;
+  }
 `;
